2d7fa656c92c07e5cba0e9b32a279dda471c5a0a
Yamen Esawii <yamenn491@gmail.com>
2025-01-03 18:57:26 +0200

    تحديث الصفحة الرئيسيه

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -9,8 +9,8 @@ const Nav = () => {
                     <li><a href="#"> <i className="fas fa-sign-in-alt">&nbsp;&nbsp;</i>تسجيل الدخول</a></li>
                     <li><a href="#">تواصل معنا<i className="fas fa-envelope"></i></a></li>
                     <li><a href="#">عن الموقع<i className="fas fa-info-circle"></i></a></li>
-                    <li><a href="#">الأسئلة المتكررة<i className="fas fa-question-circle"></i></a></li>
-                    <li><a href="#">المواد الدراسية<i className="fas fa-book-reader"></i></a></li>
+                    <li><a href="#">المواد الدراسية<i className="fas fa-question-circle"></i></a></li>
+                    <li><a href="#">التخصصات<i className="fas fa-book-reader"></i></a></li>
                     <li><a href="#">الصفحة الرئيسية<i className="fas fa-home"></i></a></li>
                 </ul>
             </nav>
